b4b9d6951cedeab76da5ee8afd677448cd03e6fa
Kangaa-Bit69 <61615273+Kangaa-Bit69@users.noreply.github.com>
2023-12-04 11:36:18 -0700

    added shake button and shake function
    also added max and min size for canvas

--- etchScript.js
+++ etchScript.js
@@ -3,18 +3,36 @@
 const grid = document.querySelector('.grid');
 const div = document.createElement('div');
 const fragment = document.createDocumentFragment();
-const pixels = (document.querySelectorAll('pixel'));
+const pixels = document.querySelectorAll('.pixel');
 div.classList.add('div');
 grid.classList.add('grid');
 
+
 //user change canvas size
 function promptGridSize() {
-    return prompt('What Size?');
+    let size = prompt('Pick your dimensions up to 100');
+    if (size < 100 && size > 1) {
+        console.log(size);
+        return size;
+        
+    } else {
+        console.log('Out of Range');
+        if (confirm('Enter a number between 1 and 100')) promptGridSize();
+        return size;
+        
+        
+    }
 }
 
 //mouseover hanldler
 function handleMouse() {
-    this.style.backgroundColor = 'black'
+    this.style.backgroundColor = 'black';
+}
+//change all pixels back to white
+function shake() {
+ for (pixel of document.querySelectorAll('.pixel')) { pixel.style.backgroundColor = 'white'}
+    
+
 }
 
 //function to greate grid 
@@ -27,15 +45,16 @@ function createGrid (gridSize) {
         //pixels
         for (let j = 0; j < gridSize; j++){
             pixel = document.createElement('div');
-            pixel.className=(`pixel`);
+            pixel.className=(`pixel`);            
+            pixel.classList.add('pixel');
             pixel.addEventListener('mouseover',  handleMouse);                                  
             row.append(pixel);
             fragment.appendChild(row);            
         } 
-    } grid.appendChild(fragment);
+    } grid.appendChild(fragment); 
     
 }
-// set initial 
+// set initial grid
 window.onload = function() {
     createGrid(16);
   };
